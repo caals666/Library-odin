0adcc439e13a0784eeddbf8f27c7d9a5408877dc
caals666 <192770810+caals666@users.noreply.github.com>
2025-11-24 19:48:43 +0530

    it works, basic features like remove, add all are working with localStorage

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 const addBook=document.getElementById("addBook");
-const bookAdd=document.getElementById("bookAdd");
 const bookTab=document.getElementById("bookTab");
 const readStatus=document.getElementById("readStatus");
-const books = [{ title: "Atomic Habits", author: "James Clear", pages: 300, read: true,id:crypto.randomUUID() },{ title: "The Midnight Library", author: "Matt Haig", pages: 304, read: false, id: crypto.randomUUID() },{title:"The Alchemist",author:"Paulo Coelho",pages:250,read:true,id:crypto.randomUUID()}];
+const addBtn=document.getElementById("addBtn");
+const bookName=document.getElementById("bookName");
+const bookAuthor=document.getElementById("bookAuthor");
+const bookPages=document.getElementById("bookPages");
+let books = [];
 
-const changeRead = (e) => {
+const changeRead = (e,x) => {
     if (e.innerHTML == "Read") {
         e.innerHTML = "Not Read";
         e.classList.remove("bg-green-500");
         e.classList.remove("hover:bg-green-600");
         e.classList.add("hover:bg-red-600");
         e.classList.add("bg-red-500");
+        x.read=false;
     } else {
         e.innerHTML = "Read";
         e.classList.remove("bg-red-500");
         e.classList.remove("hover:bg-red-600");
         e.classList.add("hover:bg-green-600");
         e.classList.add("bg-green-500");
+        x.read=true;
     }
+    books=JSON.parse(localStorage.getItem("userBooks"))||[];
+    books=books.map((book)=>book.id===x.id?{...book,read:x.read}:book);
+    localStorage.setItem("userBooks",JSON.stringify(books));
 };
-localStorage.setItem("userBooks",JSON.stringify(books));
-const user=JSON.parse(localStorage.getItem("userBooks"));
-if(user){
-    for(const x of user){
-        printDiv(x);
-    }
-}
 function printDiv(x){
     const libraryDiv=document.getElementById("bookDiv");
     const div=document.createElement("div");
@@ -43,11 +44,10 @@ function printDiv(x){
     author.classList="mt-5 text-xl text-center";
     pages.innerHTML=x.pages+" pages";
     pages.classList="mt-5 text-xl text-center";
-    console.log(x.read);
     read.innerText=`${x.read?`Read`:`Not Read`}`;
     read.classList=`mt-5 ${x.read?`bg-green-500`:`bg-red-500`} hover:cursor-pointer hover:${x.read?`bg-green-600`:`bg-red-600`} text-xl w-[100%]  mt-5 h-[40px] rounded-xl`;
     read.addEventListener("click", function() {
-        changeRead(read);
+        changeRead(read,x);
     });
     remove.innerHTML="Remove";
     remove.classList="justify-center bg-gray-400 hover:bg-gray-500 hover:cursor-pointer text-xl text-center mt-5 h-[40px] rounded-xl w-[100%]";
@@ -64,10 +64,36 @@ function printDiv(x){
 
 const removeElement=(x)=>{
     const rdiv=document.getElementById(x.id);
+    books=JSON.parse(localStorage.getItem("userBooks")) || [];
+    books=books.map((book)=>(book.id===x.id)?null:book);
+    localStorage.setItem("userBooks",JSON.stringify(books));
     rdiv.remove();
 };
 
 addBook.addEventListener("click",()=>Booktab());
 const Booktab=()=>{
     bookTab.classList.toggle("hidden");
+}
+
+addBtn.addEventListener("click", ()=>AddBtn());
+const AddBtn=()=>{
+    if(bookName.value&&bookAuthor.value&&bookPages.value){
+        const newBook={title: bookName.value, author: bookAuthor.value, pages: bookPages.value, read: readStatus.checked,id:crypto.randomUUID() };
+        books.push(newBook);
+        printDiv(newBook);
+        bookName.value="";
+        bookPages.value="";
+        bookAuthor.value="";
+        readStatus.checked=false;
+    }
+    else{
+        throw new Error("Enter all values");
+    }
+    localStorage.setItem("userBooks",JSON.stringify(books));
+}
+const user=JSON.parse(localStorage.getItem("userBooks"));
+if(user){
+    for(const x of user){
+        printDiv(x);
+    }
 }
